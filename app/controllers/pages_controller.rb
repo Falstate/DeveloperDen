630b05b8ddf3f164cd4094e9b435439256f418e8
Riche <faelstate@gmail.com>
2016-05-21 17:53:15 +0000

    Created Pages controller, Added Explore, home, index and profile pages. changed root URL to index page.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  
+  # back end code for index/pages
+  def index
+  end
+
+  # back end code for index/pages
+  def home
+  end
+
+  # back end code for index/pages
+  def profile
+  end
+
+  # back end code for index/pages
+  def explore
+  end
+end
